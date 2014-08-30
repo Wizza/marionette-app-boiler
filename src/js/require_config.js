6fd18d2c613dae44aac4f2d4e5a8f9a59625511c
Greg McDowell <greg@oxigeno.co.nz>
2014-08-31 08:51:21 +1200

    Moved require configuration into separate file to enable it to be reused in tests Removed require_main_built from repo as this is generated in build scripts (tbd)

---
+++ src/js/require_config.js
@@ -0,0 +1,37 @@
+/**
+ * Created by greg on 31/08/14.
+ */
+
+
+requirejs.config({
+    /*baseUrl: "./js/",*/
+    paths: {
+        jquery: "bower_components/jquery/dist/jquery",
+        underscore: "bower_components/underscore/underscore",
+        backbone: "bower_components/backbone/backbone",
+        "backbone.babysitter": "bower_components/backbone.babysitter/backbone.babysitter",
+        "backbone.radio": "bower_components/backbone.radio/build/backbone.radio",
+        /*"backbone.wreqr": "bower_components/backbone.wreqr/lib/backbone.wreqr",*/
+        "backbone.marionette": "bower_components/backbone.marionette/lib/backbone.marionette",
+        tpl: "bower_components/requirejs-tpl/tpl",
+        'radio.shim': "bower_components/48afb443b8c8c6ee4f46/radio.shim"
+    },
+    shim: {
+        "underscore": {
+            exports: "_"
+        },
+        backbone: {
+            deps: [
+                "jquery",
+                "underscore"
+            ],
+            exports: "Backbone"
+        },
+        "backbone.marionette": {
+            deps: [
+                "backbone"
+            ],
+            exports: "Marionette"
+        }
+    }
+});
